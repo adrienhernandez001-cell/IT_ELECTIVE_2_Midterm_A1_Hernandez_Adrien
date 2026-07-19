f54e2a6bee3cf38384af34d48b3aa1ac970f0a54
adrienhernandez001-cell <adrienhernandez001@gmail.com>
2026-07-19 14:37:48 +0800

    done

--- ConsoleApp1/WebApplication1/Controllers/HomeController.cs
+++ ConsoleApp1/WebApplication1/Controllers/HomeController.cs
@@ -6,28 +6,44 @@ namespace IT_ELECTIVE_2_Midterm_A1_Hernandez_Adrien.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Title"] = "Home";
             ViewData["Name"] = "Adrien Hernandez";
+            ViewData["Role"] = "Aspiring IT Professional";
+
+            ViewData["Intro"] =
+                "Hello! I am currently pursuing a Bachelor of Science in Information Technology. I have a passion for technology, problem-solving, and continuously learning new skills. I enjoy exploring web development, databases, networking, and system administration.";
+
             return View();
         }
 
         public IActionResult About()
         {
-            ViewData["About"] = "I am an IT student passionate about software and web development.";
+            ViewData["Title"] = "About";
+
+            ViewData["About1"] =
+                "I am currently pursuing a Bachelor of Science in Information Technology. I have a strong passion for technology and enjoy learning different areas of IT including web development, databases, networking, and system administration.";
+
+            ViewData["About2"] =
+                "My goal is to become a Professional IT Specialist who can contribute to innovative technology solutions while continuously learning and adapting to new technologies.";
+
             return View();
         }
 
         public IActionResult Skills()
         {
+            ViewData["Title"] = "Skills";
             return View();
         }
 
         public IActionResult Projects()
         {
+            ViewData["Title"] = "Projects";
             return View();
         }
 
         public IActionResult Contact()
         {
+            ViewData["Title"] = "Contact";
             return View();
         }
     }
